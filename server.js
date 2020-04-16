c9164906c7fa10509d75a7cca68d14e8a463dd61
dan <guaneec@gmail.com>
2020-04-16 22:22:29 +0800

    Use tmp files instead

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const https = require('https')
 const fs = require('fs')
 const { exec } = require("child_process");
 const request = require('request');
+const tmp = require('tmp');
 
 const fileUpload = require('express-fileupload');
 
@@ -12,6 +13,7 @@ const port = 5999
 const ffmpeg = '/home/uidd2020/user/tilde/ffmpeg/build/bin/ffmpeg ';
 
 function processBody(s) {
+    console.log(s);
     for (let line of s.split('\n')) {
         if (!line.trim().length) continue;
         const s = JSON.parse(line);
@@ -20,18 +22,23 @@ function processBody(s) {
     }
 }
 
-function getText(resolve, reject) {
-    request({
-        url: 'https://www.google.com/speech-api/v2/recognize?output=json&lang=zh-TW&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'audio/x-flac; rate=44100'
-        },
-        body: fs.readFileSync('out.flac'),
-        encoding: null,
-    }, function(err, res, body) {
-        if (err) reject(err);
-        else resolve(processBody(body.toString('utf8')));
+function getText(path) {
+    return new Promise((resolve, reject) => {
+        request({
+            url: 'https://www.google.com/speech-api/v2/recognize?output=json&lang=zh-TW&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'audio/x-flac; rate=44100'
+            },
+            body: fs.readFileSync(path),
+            encoding: null,
+        }, function (err, res, body) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(processBody(body.toString('utf8')));
+        });
     });
 }
 
@@ -40,27 +47,28 @@ function getText(resolve, reject) {
 app.use(express.static('static'))
 app.use(fileUpload());
 
+
 app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/speech', (req, res) => {
     try {
-        const path = './out.ogg';
-        const flacpath = './out.flac';
-        console.log(req.files);
-        fs.unlink(flacpath, (e) => { if (e) throw e });
+        const path = `${tmp.tmpNameSync()}.ogg`;
+        const flacPath = `${tmp.tmpNameSync()}.flac`;
+        console.log(path, flacPath);
         fs.writeFileSync(path, req.files.data.data);
-        exec(`${ffmpeg} -i ${path} -ar 44100 ${flacpath}`, err => {
+        exec(`${ffmpeg} -i ${path} -ar 44100 ${flacPath}`, err => {
             if (err) throw err;
-            getText((transcript) => {
-                console.log(transcript);
-                res.json({
-                    transcript,
-                })
-            }, (err) => {
-                throw err;
-            })
+            getText(flacPath).then(
+                (transcript) => {
+                    console.log(transcript);
+                    res.json({
+                        transcript,
+                    })
+                }
+            ).catch(err => { throw err }).finally(() => {
+                fs.unlinkSync(path);
+                fs.unlinkSync(flacPath);
+            });
         });
-
-
     } catch (e) {
         console.error(e);
         res.status(401).send('error');
