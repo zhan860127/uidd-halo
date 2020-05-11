157685c8f6103d38dd59cedc3389fb482b3f6ff7
Dan <guaneec@gmail.com>
2020-05-11 22:15:19 +0800

    Add navigation with logo

--- layouts/parent.vue
+++ layouts/parent.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <b-navbar type="dark" variant="dark">
-      <b-navbar-brand> HALO </b-navbar-brand>
+      <b-navbar-brand>
+        <nuxt-link
+          :to="
+            $route.query.c ? `/parent?c=${$route.query.c}` : `/parent/children`
+          "
+          ><img
+            class="img-fuild"
+            style="height: 30px;"
+            src="~/assets/img/halo-logo.png"
+            alt=""
+        /></nuxt-link>
+      </b-navbar-brand>
       <b-navbar-nav class="ml-auto">
         <b-button v-b-toggle.side-menu> Menu </b-button>
       </b-navbar-nav>
