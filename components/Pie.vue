619ec70c9c61b0a7c514866203ce60d92d23d545
Dan <guaneec@gmail.com>
2020-05-17 23:29:37 +0800

    Add Pie component

---
+++ components/Pie.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div
+      class="ring"
+      :class="{ half: v > 0.5 }"
+      :style="{ '--d': `${v * 360 - 90}deg`, '--bg': bg, '--fg': fg }"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class classname extends Vue {
+  @Prop({ default: 'grey' }) bg!: string;
+  @Prop({ default: 'purple' }) fg!: string;
+  @Prop({ default: 0 }) v!: number;
+}
+</script>
+
+<style lang="scss">
+.ring {
+  background: linear-gradient(var(--d), var(--bg) 50%, transparent 50%),
+    linear-gradient(to right, var(--bg) 50%, var(--fg) 50%);
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+  &.half {
+    background: linear-gradient(var(--d), transparent 50%, var(--fg) 50%),
+      linear-gradient(to right, var(--bg) 50%, var(--fg) 50%);
+  }
+}
+</style>
