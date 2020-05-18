77946c588159d87c7633342a81bb3afc2189cf74
Dan <guaneec@gmail.com>
2020-05-18 09:42:27 +0800

    Add nav dropdown

--- layouts/parent.vue
+++ layouts/parent.vue
@@ -10,7 +10,32 @@
           ><b-icon-house-door-fill style="height: auto; width: 30px;"
         /></nuxt-link>
       </b-navbar-brand>
-      <b-navbar-nav class="ml-auto">
+      <b-navbar-nav class="flex-grow-1">
+        <b-nav-item-dropdown
+          v-if="title"
+          ref="dropdown"
+          :text="title"
+          class="mx-auto nav-dropdown"
+        >
+          <nuxt-link
+            class="dropdown-item"
+            :to="`/parent/logs?c=${$route.query.c}`"
+            @click="closeDropdown"
+            >Recordings</nuxt-link
+          >
+          <nuxt-link
+            class="dropdown-item"
+            :to="`/parent/keywords?c=${$route.query.c}`"
+            >Response</nuxt-link
+          >
+          <nuxt-link
+            class="dropdown-item"
+            :to="`/parent/call?c=${$route.query.c}`"
+            >Call</nuxt-link
+          >
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+      <b-navbar-nav>
         <b-icon-list v-b-toggle.side-menu style="height: 27px; width: 27px;" />
       </b-navbar-nav>
     </b-navbar>
@@ -19,7 +44,33 @@
   </div>
 </template>
 
-<style>
+<script lang="ts">
+import { Vue, Component, Ref, Watch } from 'vue-property-decorator';
+import { BDropdown } from 'bootstrap-vue';
+import '~/assets/scss/_fonts.scss';
+
+@Component
+export default class classname extends Vue {
+  get title(): string {
+    const p = this.$route.path;
+    return p.startsWith('/parent/logs')
+      ? 'Recordings'
+      : p.startsWith('/parent/keywords')
+      ? 'Response'
+      : p.startsWith('/parent/call')
+      ? 'Call'
+      : '';
+  }
+
+  @Ref('dropdown') dropdown!: BDropdown;
+  @Watch('$route')
+  closeDropdown() {
+    if (this.dropdown) this.dropdown.hide();
+  }
+}
+</script>
+
+<style lang="scss">
 html,
 body {
   scroll-behavior: smooth;
@@ -40,5 +91,18 @@ body {
   color: inherit;
   text-decoration: inherit;
   cursor: inherit;
+}
+
+.nav-dropdown .dropdown-menu {
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.nav-dropdown {
+  font: 20px/27px 'Avenir Book';
+  font-weight: bold;
+  & .nav-link {
+    color: #fcf6ef !important;
+  }
 }
 </style>
