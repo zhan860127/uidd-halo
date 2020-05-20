38a94de6d86a9dd37ceb1d01237c3ef4da6dd1c4
liu15498 <61860096+liu15498@users.noreply.github.com>
2020-05-20 11:46:22 +0800

    Add files via upload

---
+++ update./index.vue
@@ -0,0 +1,193 @@
+
+<template>
+  <div>
+     <nuxt-link :to="{ path: '/parent/connect', query: { c: $route.query.c } }"
+      >Link child's device</nuxt-link
+    >
+ <div class="wrapper">
+    <div class="menu con"  v-touch:swipe.left="left"
+    v-touch:swipe.right="right" >
+      
+        <nuxt-link
+          :to="{ path: '/parent/keywords', query: { c: $route.query.c } }"
+          ><img ref="1" class="link p1" src="~/assets/img/m/1-11.png"/></nuxt-link
+        >
+    
+     
+        <nuxt-link :to="{ path: '/parent/logs', query: { c: $route.query.c } }"
+          ><img ref="2" class="link p2"  src="~/assets/img/m/1-02.png"/></nuxt-link
+        >
+     
+        <nuxt-link :to="{ path: '/parent/call', query: { c: $route.query.c } }"
+          ><img  ref="3" class="link p3" src="~/assets/img/m/1-10.png"/></nuxt-link
+        >
+    
+      <div ref="4" id="contain" class="link p4">
+       
+      <div id="name">{{ child.name }}</div>
+       <nuxt-link :to="{ path: '/parent/children' }"
+      ><img id="pic"  src="~/assets/img/m/1-12.png"/></nuxt-link>
+      </div>
+    </div>
+  </div>
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Vue2 from 'vue'
+import Vue2TouchEvents from 'vue2-touch-events'
+
+Vue2.use(Vue2TouchEvents, {
+    disableClick: false,
+    touchClass: '',
+    tapTolerance: 10,
+    touchHoldTolerance: 400,
+    swipeTolerance: 30,
+    longTapTimeInterval: 400
+})
+@Component({
+  async asyncData({ $axios, query, error }) {
+    try {
+      const child = await $axios.$get('/api/parent/child', {
+        params: { id: query.c },
+      });
+      return { child };
+    } catch (e) {
+      error({ statusCode: 401 });
+    }
+  },
+  watchQuery: true,
+  layout: 'parent',
+})
+export default class menu extends Vue {
+        
+        left () {
+            console.log("left");
+            var p1=document.getElementsByClassName("p1")[0];
+            var p2=document.getElementsByClassName("p2")[0];
+            var p3=document.getElementsByClassName("p3")[0];
+            var p4=document.getElementsByClassName("p4")[0];
+           
+            setTimeout(function() { 
+            p1.classList.remove('p1');
+            p2.classList.remove('p2');
+            p3.classList.remove('p3');
+            p4.classList.remove('p4');
+            p1.classList.add('p2');
+            p2.classList.add('p4');
+            p3.classList.add('p1');
+            p4.classList.add('p3');}, 100);
+        }
+        right () {
+            console.log("left")  
+            var p1=document.getElementsByClassName("p1")[0];
+            var p2=document.getElementsByClassName("p2")[0];
+            var p3=document.getElementsByClassName("p3")[0];
+            var p4=document.getElementsByClassName("p4")[0];
+           
+            setTimeout(function() { 
+            p1.classList.remove('p1');
+            p2.classList.remove('p2');
+            p3.classList.remove('p3');
+            p4.classList.remove('p4');
+            p1.classList.add('p3');
+            p2.classList.add('p1');
+            p3.classList.add('p4');
+            p4.classList.add('p2');}, 100);
+        }
+        
+    
+  
+   
+}
+
+</script>
+
+<style >
+.link {
+transform: translate(-50%,-50%);
+  max-height: 300px;
+  max-width: 220px;
+}
+body{
+  margin: 0%;
+  padding: 0%;
+}
+.con{
+  min-width: 300px;
+  width: 414px;
+  height: 100%;}
+.menu{
+   position: relative;
+   display:flex;
+     margin:0px;
+    
+     
+    height: 90vh;
+    width: 500px;
+   
+}
+.wrapper {
+  max-width: 414px;
+  margin: 0 auto;
+  position: relative;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-left: 58px;
+  padding-right: 58px;
+}
+#contain{
+    position: relative;
+    width:250px;
+}
+#name{
+ position: absolute;
+  bottom: 47%;
+  left:20%;
+  font-size:30px;
+  z-index:1;
+}
+#pic{
+ position: absolute;
+  max-height: 300px;
+  max-width: 220px;
+}
+.p1{
+  z-index:2;
+  position: absolute;
+  top:63%;
+  left:55%;
+  transition-duration: 1s;
+
+}
+.p2{
+  z-index:1;
+  position: absolute;
+   top:48%;
+  left:25%;
+  transition-duration: 1s;
+
+}
+.p3{
+  z-index:1;
+  position: absolute;
+  top:38%;
+  left:75%;
+  transition-duration: 1s;
+
+}
+.p4{
+  z-index:0;
+position: absolute;
+ top:28%;
+  left:40%;
+  transition-duration: 1s;
+
+}
+</style>
---
+++ update./sign_up.vue
@@ -0,0 +1,183 @@
+<template>
+  <div id=root>
+  <div class="wrapper">
+    <div class="con">
+  <div>
+    <p id="t1">SIGN UP</p>
+    <p id="t2">Creat your Account</p>
+
+    <form id="form" method="POST" action="/api/register">
+      <input
+        id="email"
+        class="input"
+        type="text"
+        name="email"
+        placeholder="Email"
+      />
+      <br />
+      <input
+        id="user"
+        class="input"
+        type="text"
+        name="username"
+        placeholder="Username"
+      />
+      <br />
+      <input
+        id="psd"
+        class="input"
+        type="password"
+        name="password"
+        placeholder="Password"
+      />
+      <br />
+      <input id="psd2" class="input" type="password" placeholder="Confirm" />
+      <br />
+      <input class="output" type="submit" value="DONE" />
+    </form>
+    <div id="r"></div>
+    <img id="i01" src="~/assets/img/1.png" onclick="history.back()" />
+  </div>
+    </div></div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class classname extends Vue {}
+</script>
+
+<style lang="sass">
+body
+  position: relative
+  top: 0px
+  left: 0px
+  width: 100%
+  min-width: 100px
+  height: 100%
+  background: #FFFFFF 0% 0% no-repeat padding-box
+  margin: 0px
+  opacity: 1
+
+#root 
+  height: 100vh
+  min-height: 450px
+  display: flex
+  flex-direction: column
+
+.wrapper 
+  max-width: 414px
+  margin: 0 auto
+  position: relative
+  height: 100%
+  display: flex
+  flex-direction: column
+  align-items: center
+  justify-content: center
+  padding-left: 58px
+  padding-right: 58px
+
+#t1
+  position: absolute
+  top: 135px;
+  left: 134px;
+  width: 150px;
+  height: 48px;
+  text-align: left
+  font-size: 40px
+  font-family: Seravek
+  letter-spacing: 0px
+  color: #4D6790
+  opacity: 1
+  margin: 0px
+
+#t2
+  position: absolute
+  top: 183px;
+  left: 129px;
+  width: 176px;
+  height: 29px;
+  text-align: left
+  font-family: Seravek
+  font-size: 20px
+  letter-spacing: 0px
+  color: #4D6790
+  opacity: 1
+  margin: 0px
+
+#i01
+  position: absolute
+  bottom: 30px;
+  left: 377px;
+  width: 23px;
+  height: 23px;
+  opacity: 1
+
+.input
+  border-radius: 30px
+  font-size: 20px
+  width: 316px;
+  height: 44px;
+  padding: 20px
+  font: Seravek
+  background: #F5F4F4 0% 0% no-repeat padding-box
+  border: 0px
+.output
+  border-radius: 30px
+  position: absolute
+  top: 531px;
+  left: 49px;
+  width: 316px;
+  height: 44px;
+
+  font-size: 20px
+
+  text-align: middle
+  font: Seravek
+  color: #FFFFFF
+  background: #4D6790 0% 0% no-repeat padding-box
+  border: 0px
+
+::placeholder
+  color: rgb(107, 143, 201)
+
+#email
+  position: absolute
+  top: 241px;
+  left: 49px;
+
+#user
+  position: absolute
+  top: 314px;
+  left: 49px;
+
+#psd
+  position: absolute
+  top: 386px;
+  left: 49px;
+
+#psd2
+  position: absolute
+  top: 459px;
+  left: 49px;
+
+.mes
+  position: absolute
+  text-align: center
+  color: #4D6790
+  font-family: Seravek
+  font-size: 4.83vw
+  top: 150.48vw
+  left: 50%
+  -webkit-transform: translate(-50%, -50%)
+
+.con
+  min-width: 300px
+  width: 414px;
+  height: 100%
+
+@font-face
+  font-family: 'Seravek'
+  src: url('~assets/font/SeravekBasic.ttf') format("truetype")
+</style>
