a380d1922d657f490cc8692180e3f42971e83e0a
liu15498 <61860096+liu15498@users.noreply.github.com>
2020-06-18 17:29:22 +0800

    Update sign_in.vue

--- pages/sign_in.vue
+++ pages/sign_in.vue
@@ -1,40 +1,47 @@
 <template>
-  <div id="root">
-    <div class="wrapper">
-      <div class="con">
-        <p id="t1">SIGN IN</p>
-        <p id="t2">Use your Accout</p>
-        <fo<template>
   <div class="root">
     <div class="wrapper">
       <div class="con">
-        <p id="t1">SIGN IN</p>
-        <p id="t2">Use your Accout</p>
-        <form id="form" action="/api/login" method="POST">
-          <input
-            id="user"
-            class="input"
-            type="text"
-            name="username"
-            placeholder="Username"
-          />
-          <input
-            id="psd"
-            class="input"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-          <input
-            class="output"
-            type="submit"
-            value="DONE"
-            onclick="log(); return false;"
-          />
-        </form>
-        <div id="r"></div>
-
-        <img id="i01" src="~/assets/img/1.png" onclick="history.back()" />
+        <div>
+          <p id="t1">SIGN UP</p>
+          <p id="t2">Creat your Account</p>
+          <form id="form" method="POST" action="/api/register">
+            <input
+              id="email"
+              class="input"
+              type="text"
+              name="email"
+              placeholder="Email"
+            />
+            <br />
+            <input
+              id="user"
+              class="input"
+              type="text"
+              name="username"
+              placeholder="Username"
+            />
+            <br />
+            <input
+              id="psd"
+              class="input"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <br />
+            <input
+              id="psd2"
+              class="input"
+              type="password"
+              placeholder="Confirm"
+            />
+            <br />
+            <input class="output" type="submit" value="DONE" />
+          </form>
+          <div id="r"></div>
+          <img id="i01" src="~/assets/img/1.png" onclick="history.back()" />
+        </div>
       </div>
     </div>
   </div>
@@ -59,23 +66,16 @@ export default class classname extends Vue {
 </script>
 
 <style lang="sass" scoped>
-@font-face
-  font-family: 'Seravek'
-  src: url('~assets/font/SeravekBasic.ttf') format("truetype")
-
 body
   position: relative
   top: 0px
-  left: 0
+  left: 0px
   width: 100%
   min-width: 100px
   height: 100%
   background: #FFFFFF 0% 0% no-repeat padding-box
   margin: 0px
   opacity: 1
-
-
-
 
 .root
   height: 100vh
@@ -85,7 +85,7 @@ body
   flex-direction: column
 
 .wrapper
-  max-width: 414px
+  max-width: 100%
   margin: 0 auto
   position: relative
   height: 100%
@@ -98,9 +98,9 @@ body
 
 #t1
   position: absolute
-  top: 165px;
-  left: 138px;
-  width: 146px;
+  top: 135px;
+  left: 134px;
+  width: 150px;
   height: 48px;
   text-align: left
   font-size: 40px
@@ -112,9 +112,9 @@ body
 
 #t2
   position: absolute
-  top: 213px;
-  left: 135px;
-  width: 157px;
+  top: 183px;
+  left: 129px;
+  width: 176px;
   height: 29px;
   text-align: left
   font-family: Seravek
@@ -124,19 +124,9 @@ body
   opacity: 1
   margin: 0px
 
-.mes
-  position: absolute
-  text-align: center
-  color: #4D6790
-  font-family: Seravek
-  font-size: 4.83%
-  top: 120.48%
-  left: 50%
-  -webkit-transform: translate(-50%, -50%)
-
 #i01
   position: absolute
-  bottom: 41px;
+  bottom: 30px;
   left: 377px;
   width: 23px;
   height: 23px;
@@ -144,24 +134,25 @@ body
 
 .input
   border-radius: 30px
+  font-size: 20px
   width: 316px;
   height: 44px;
-  font-size: 20px
-  padding: 4.83%
-  box-sizing: border-box
+  padding: 20px
   font: Seravek
   background: #F5F4F4 0% 0% no-repeat padding-box
   border: 0px
 .output
   border-radius: 30px
   position: absolute
-  top: 416px;
+  top: 531px;
   left: 49px;
   width: 316px;
   height: 44px;
+
   font-size: 20px
+
   text-align: middle
-  font-family: Seravek
+  font: Seravek
   color: #FFFFFF
   background: #4D6790 0% 0% no-repeat padding-box
   border: 0px
@@ -169,182 +160,42 @@ body
 ::placeholder
   color: rgb(107, 143, 201)
 
-#user
+#email
   position: absolute
-  top: 271px;
+  top: 241px;
   left: 49px;
 
+#user
+  position: absolute
+  top: 314px;
+  left: 49px;
 
 #psd
   position: absolute
-  top: 344px;
+  top: 386px;
   left: 49px;
+
+#psd2
+  position: absolute
+  top: 459px;
+  left: 49px;
+
+.mes
+  position: absolute
+  text-align: center
+  color: #4D6790
+  font-family: Seravek
+  font-size: 4.83vw
+  top: 150.48vw
+  left: 50%
+  -webkit-transform: translate(-50%, -50%)
 
 .con
   min-width: 300px
   width: 414px;
   height: 100%
-</style>
-rm id="form" action="/api/login" method="POST">
-          <input
-            id="user"
-            class="input"
-            type="text"
-            name="username"
-            placeholder="Username"
-          />
-          <input
-            id="psd"
-            class="input"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-          <input
-            class="output"
-            type="submit"
-            value="DONE"
-            onclick="log(); return false;"
-          />
-        </form>
-        <div id="r"></div>
 
-        <img id="i01" src="~/assets/img/1.png" onclick="history.back()" />
-      </div>
-    </div>
-  </div>
-</template>
-
-<script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
-
-@Component
-export default class classname extends Vue {}
-</script>
-
-<style lang="sass" scoped>
 @font-face
   font-family: 'Seravek'
   src: url('~assets/font/SeravekBasic.ttf') format("truetype")
-
-body
-  position: relative
-  top: 0px
-  left: 0
-  width: 100%
-  min-width: 100px
-  height: 100%
-  background: #FFFFFF 0% 0% no-repeat padding-box
-  margin: 0px
-  opacity: 1
-
-
-
-
-#root
-  height: 100vh
-  min-height: 450px
-  display: flex
-  flex-direction: column
-
-.wrapper
-  max-width: 414px
-  margin: 0 auto
-  position: relative
-  height: 100%
-  display: flex
-  flex-direction: column
-  align-items: center
-  justify-content: center
-  padding-left: 58px
-  padding-right: 58px
-
-#t1
-  position: absolute
-  top: 165px;
-  left: 138px;
-  width: 146px;
-  height: 48px;
-  text-align: left
-  font-size: 40px
-  font-family: Seravek
-  letter-spacing: 0px
-  color: #4D6790
-  opacity: 1
-  margin: 0px
-
-#t2
-  position: absolute
-  top: 213px;
-  left: 135px;
-  width: 157px;
-  height: 29px;
-  text-align: left
-  font-family: Seravek
-  font-size: 20px
-  letter-spacing: 0px
-  color: #4D6790
-  opacity: 1
-  margin: 0px
-
-.mes
-  position: absolute
-  text-align: center
-  color: #4D6790
-  font-family: Seravek
-  font-size: 4.83%
-  top: 120.48%
-  left: 50%
-  -webkit-transform: translate(-50%, -50%)
-
-#i01
-  position: absolute
-  bottom: 41px;
-  left: 377px;
-  width: 23px;
-  height: 23px;
-  opacity: 1
-
-.input
-  border-radius: 30px
-  width: 316px;
-  height: 44px;
-  font-size: 20px
-  padding: 4.83%
-  box-sizing: border-box
-  font: Seravek
-  background: #F5F4F4 0% 0% no-repeat padding-box
-  border: 0px
-.output
-  border-radius: 30px
-  position: absolute
-  top: 416px;
-  left: 49px;
-  width: 316px;
-  height: 44px;
-  font-size: 20px
-  text-align: middle
-  font-family: Seravek
-  color: #FFFFFF
-  background: #4D6790 0% 0% no-repeat padding-box
-  border: 0px
-
-::placeholder
-  color: rgb(107, 143, 201)
-
-#user
-  position: absolute
-  top: 271px;
-  left: 49px;
-
-
-#psd
-  position: absolute
-  top: 344px;
-  left: 49px;
-
-.con
-  min-width: 300px
-  width: 414px;
-  height: 100%
 </style>
